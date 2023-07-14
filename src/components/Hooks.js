93fb62b304e932d025f493fa71a6634b666d2cb3
Alberto Mario Guerrero G <psico.amgg@gmail.com>
2023-07-14 15:29:02 -0500

    Hooks

---
+++ src/components/Hooks.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Hooks = () => {
+  return (
+    <details>
+      <summary>CARACTERISTICAS</summary>
+      <h2> Son los actual... </h2>
+      <ol>
+        <lil>
+          {" "}
+          A pesar que hoy en día se unan más los hooks el inicio de react fue
+          con class component
+        </lil>{" "}
+        <br />
+        <lil> Significa gancho</lil> <br />
+        <lil> Nace en React 16.8 </lil> <br />
+        <lil>
+          {" "}
+          Permite enganchar por medio de funciones el estado y ciclo de vida{" "}
+        </lil>{" "}
+        <br />
+        <lil>
+          {" "}
+          Antes los componentes funcionales eran contenedores y/o tontos{" "}
+        </lil>{" "}
+        <br />
+        <lil>
+          {" "}
+          Antes los componentes funcionales eran contenedores y/o tontos{" "}
+        </lil>{" "}
+        <br />
+      </ol>
+    </details>
+  );
+};
+
+export default Hooks;
